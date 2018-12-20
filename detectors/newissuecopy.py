46f12285ce13e94539a82f6a701512be14a388bf
Ernest W. Durbin III <ewdurbin@gmail.com>
2018-12-20 13:20:15 -0500

    initial import

---
+++ detectors/newissuecopy.py
@@ -0,0 +1,20 @@
+from roundup import roundupdb
+
+def newissuecopy(db, cl, nodeid, oldvalues):
+    ''' Copy a message about new issues to a team address.
+    '''
+    # so use all the messages in the create
+    change_note = cl.generateCreateNote(nodeid)
+
+    # send a copy to the nosy list
+    for msgid in cl.get(nodeid, 'messages'):
+        try:
+            # note: last arg must be a list
+            cl.send_message(nodeid, msgid, change_note,
+                ['roundup-devel@lists.sourceforge.net'])
+        except roundupdb.MessageSendError as message:
+            raise roundupdb.DetectorError, message
+
+def init(db):
+    db.issue.react('create', newissuecopy)
+#SHA: 6ed003c947e1f9df148f8f4500b7c2e68a45229b
